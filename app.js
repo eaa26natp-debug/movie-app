a8975c263c2f61d076b3103b95e1c5517ed0c5f3
eaa26natp-debug <eaa26natp@students.eaaa.dk>
2026-04-06 10:11:35 +0200

    commit: føste ændringer

---
+++ app.js
@@ -0,0 +1,6 @@
+document.body.innerHTML += "<p>Hello JavaScript.</p>";
+//alert("Hello Multimedia Designers.");
+console.log("Hello Multimedia Designers.");
+console.log("This is a message in the console.");
+console.log("This is your first JavaScript App 🎉");
+console.log("Hi Students!");
