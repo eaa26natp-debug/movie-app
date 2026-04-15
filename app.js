e72641086a00f35dca34c2317f1aec63dc0df24a
eaa26natp-debug <eaa26natp@students.eaaa.dk>
2026-04-15 09:42:53 +0200

    Commit: DAG 2 - færdig med Opgave 2 (arrays og loops)

--- app.js
+++ app.js
@@ -1,88 +1,31 @@
 "use strict";
 
-console.log("App starter...");
+console.log("Movie App - DAG 2 starter...");
 
-// // Find knappen
-// const clickButton = document.querySelector("#click-button");
+const movies = ["Inception", "The Matrix", "Interstellar", "The Dark Knight"];
 
-// // Lyt efter klik
-// clickButton.addEventListener("click", function () {
-//   console.log("Knappen blev klikket! ");
-// });
+const movieList = document.querySelector("#movie-list");
+console.log(movieList);
 
-const clickButton = document.querySelector("#click-button");
-const heading = document.querySelector("h1");
+// console.log("Alle film:", movies);
 
-clickButton.addEventListener("click", function () {
-  console.log("Klik! ");
-  //   heading.textContent = "Hej med Dig! ";
-  clickButton.style.backgroundColor = "blue";
-  clickButton.style.color = "white";
-  //   heading.style.color = "yellow";
-});
 
-const resetbutton = document.querySelector("#reset-button");
+// console.log("Første film:", movies[0]);
+// console.log("Anden film:", movies[1]);
+// console.log("Sideste film:", movies[3]);
+// console.log("tinde film:", movies[10]);
+// console.log("Antal film:", movies.length)
 
-resetbutton.addEventListener("click", function () {
-  console.log("Reset knappen blev klikket! ");
-  //   heading.textContent = "Movie App ";
-  //   clickButton.style.backgroundColor = "none";
-  //   clickButton.style.color = "white";
-  //   heading.style.color = "white";
-});
+for(const movie of movies) {
+  const html = /* html */ `
+  <article class="movie-card">
+    <div class="movie-card">  
+      <h3>${movie}</h3>
+    </div>
+  </article> 
+  `;
+  movieList.insertAdjacentHTML("beforeend", html);
+}
 
-// Ændr count tallet
-const countDisplay = document.querySelector("#counter");
-let count = 0;
 
-clickButton.addEventListener("click", function () {
-  count = count + 1;
-  countDisplay.textContent = count;
 
-  if (count > 10) {
-    countDisplay.style.color = "lightgreen";
-  }
-  if (count === 10) {
-    alert("Tillykke! Du nåde 10 klik! ");
-  }
-});
-
-resetbutton.addEventListener("click", function () {
-  count = 0;
-  countDisplay.textContent = count;
-
-  countDisplay.style.color = "#ffd700";
-});
-
-const minusButton = document.querySelector("#minus-btn");
-
-minusButton.addEventListener("click", function () {
-  count = count - 1;
-  countDisplay.textContent = count;
-
-  if (count < 10) {
-    countDisplay.style.color = "#ffd700";
-    } 
-
-});
-
-// const minusButton = document.querySelector("#minus-btn");
-
-// function increaseCount() {
-//   count = count + 1;
-//   countDisplay.textContent = count;
-// }
-
-// function decreaseCount() {
-//   count = count - 1;
-//   countDisplay.textContent = count;
-// }
-
-// function resetCount() {
-//   count = 0;
-//   countDisplay.textContent = count;
-// }
-
-// clickButton.addEventListener("click", increaseCount);
-// minusButton.addEventListener("click", decreaseCount);
-// resetButton.addEventListener("click", resetCount);
